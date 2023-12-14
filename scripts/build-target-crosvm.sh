a7bf3a5ffc803bac1ef8b9ec535d19674e78b106
Viktor Rosendahl <viktor.rosendahl@unikie.com>
2023-12-14 12:26:26 +0200

    build-target-crosvm.sh: build crosvm for arm64
    This project is about running pkvm on arm64, so we want a crosvm that runs
    on that platform. Thus, build it for arm64. The build-target-qemu.sh
    is used as a template.
    
    Signed-off-by: Viktor Rosendahl <viktor.rosendahl@unikie.com>

--- scripts/build-target-crosvm.sh
+++ scripts/build-target-crosvm.sh
@@ -1,12 +1,128 @@
-#!/bin/bash -e
-
-export CROSVMDIR=$BASE_DIR/crosvm
-
-cd $CROSVMDIR
-git submodule update --init
+#!/bin/bash
 
 #
-# If you don't have the tools, see './tools/install-deps'
+# Tested on ubuntu 20+. You need to have qemu-user-static and binfmt-support
+# installed.
 #
-cargo build --target aarch64-linux-gnu --features=gdb
-cp target/debug/crosvm $BASE_DIR/images/guest
+
+TOOLDIR=$BASE_DIR/buildtools
+QEMU_USER=`which qemu-aarch64-static`
+
+#
+# Default: dynamic, opengl, spice, virgl, hybris
+#
+
+UBUNTU_BASE=http://cdimage.debian.org/mirror/cdimage.ubuntu.com/ubuntu-base/releases/22.04/release/ubuntu-base-22.04-base-arm64.tar.gz
+PKGLIST=`cat $BASE_DIR/scripts/package.list.22`
+
+#
+# Note: cross-compilation is also possible, these can be passed through.
+#
+unset CC
+unset LD
+unset CXX
+unset AR
+unset CPP
+unset CROSS_COMPILE
+unset CFLAGS
+unset LDFLAGS
+unset ASFLAGS
+unset INCLUDES
+unset WARNINGS
+unset DEFINES
+
+export PATH=$TOOLDIR/bin:$TOOLDIR/usr/bin:/bin:/usr/bin
+export CHROOTDIR=$BASE_DIR/oss/ubuntu
+
+NJOBS=`nproc`
+REPO=`which repo`
+
+set -e
+
+do_unmount()
+{
+	if [[ $(findmnt -M "$1") ]]; then
+		sudo umount $1
+		if [ $? -ne 0 ]; then
+			echo "ERROR: failed to umount $1"
+			exit 1
+		fi
+	fi
+}
+
+do_unmount_all()
+{
+	[ -n "$LEAVE_MOUNTS" ] && echo "leaving bind mounts in place." && exit 0
+
+	echo "Unmount all binding dirs"
+	do_unmount $CHROOTDIR/build/crosvm
+	do_unmount $CHROOTDIR/proc
+	do_unmount $CHROOTDIR/dev
+}
+
+do_clean()
+{
+	do_unmount_all
+	cd $BASE_DIR/crosvm; sudo git clean -xfd || true
+}
+
+do_distclean()
+{
+	do_unmount_all
+	cd $BASE_DIR/crosvm; sudo git clean -xfd || true
+	sudo rm -rf $CHROOTDIR
+}
+
+do_sysroot()
+{
+	mkdir -p $CHROOTDIR/build
+	if [ -e $CHROOTDIR/bin/bash ]; then
+		sudo mount --bind /dev $CHROOTDIR/dev
+		sudo mount -t proc none $CHROOTDIR/proc
+		return;
+	fi
+
+	cd $CHROOTDIR
+	wget -c $UBUNTU_BASE
+	tar xf `basename $UBUNTU_BASE`
+	sudo mount --bind /dev $CHROOTDIR/dev
+	sudo mount -t proc none $CHROOTDIR/proc
+	echo "nameserver 8.8.8.8" > etc/resolv.conf
+	cp $QEMU_USER usr/bin
+	sudo chmod a+rwx tmp
+	DEBIAN_FRONTEND=noninteractive sudo -E chroot $CHROOTDIR apt-get update
+	DEBIAN_FRONTEND=noninteractive sudo -E chroot $CHROOTDIR apt-get -y install $PKGLIST
+#	sudo -E chroot $CHROOTDIR update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 10
+#	sudo -E chroot $CHROOTDIR update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-10 10
+	rm `basename $UBUNTU_BASE`
+}
+
+do_crosvm()
+{
+	#
+	# Build always
+	#
+	mkdir -p $CHROOTDIR/build/crosvm
+	sudo mount --bind $BASE_DIR/crosvm $CHROOTDIR/build/crosvm
+	cd $CHROOTDIR/build/crosvm
+
+	sudo -E chroot $CHROOTDIR sh -c "cd /build/crosvm; cargo build --features=gdb; install target/debug/crosvm /usr/bin"
+}
+
+
+if [[ "$#" -eq 1 ]] && [[ "$1" == "clean" ]]; then
+	do_clean
+        exit 0
+fi
+if [[ "$#" -eq 1 ]] && [[ "$1" == "distclean" ]]; then
+	do_distclean
+        exit 0
+fi
+
+trap do_unmount_all SIGHUP SIGINT SIGTERM EXIT
+
+do_sysroot
+do_crosvm
+cd $BASE_DIR
+
+echo "All ok!"
